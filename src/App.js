9fd720c417740cdf46e717ea6f4df3148a374cc6
Nickautomatics <45819370+Nickautomatics@users.noreply.github.com>
2021-07-30 15:35:08 +0800

    added redirect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from "react";
 import Homepage from "./pages/homepage/homepage.component";
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/heeader/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
@@ -44,16 +44,19 @@ class App extends React.Component{
           <Switch>
               <Route exact path='/' component={Homepage} />
               <Route path='/shop' component={ShopPage} />
-              <Route path='/signin' component={SignInAndSignUpPage} />
+              <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to="/" />) : (<SignInAndSignUpPage />)} />
           </Switch>
         </div>
       );
     }
-
 }
+
+const mapStateToProps = ({user}) => ({
+    currentUser: user.currentUser
+})
 
 const mapDispatchToProps = dispatch => ({
 setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
